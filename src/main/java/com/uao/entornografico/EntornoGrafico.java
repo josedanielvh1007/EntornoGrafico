4d9a9a44a3d06627f94cc3ae2ff968d737ee9856
Jose Daniel <josedanielvh1007@gmail.com>
2025-10-15 10:11:50 -0500

    First commit

---
+++ src/main/java/com/uao/entornografico/EntornoGrafico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.uao.entornografico;
+
+/**
+ *
+ * @author jose
+ */
+public class EntornoGrafico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
